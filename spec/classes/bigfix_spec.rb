93ce2f7fee196e8e754c1e024536265291e9113d
stack72 <public@paulstack.co.uk>
2014-02-21 11:19:49 +0000

    Starting to add some specs to test the module

---
+++ spec/classes/bigfix_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe 'bigfix', :type => :class do
+
+  let(:facts) { {
+      :osfamily  => 'Windows'
+  } }
+  let(:params) {{
+      :package_source  => '',
+      :package_version => ''
+  }}
+
+  it { should contain_class('bigfix::install') }
+
+
+  context 'using params defaults' do
+    it { should contain_class('bigfix') }
+    it { should contain_package('BigFix Enterprise Client')
+      .with_ensure('installed')
+      .with_provider('windows')
+      .with_source('')
+    }
+
+  end
+
+  context 'installing a custom version' do
+
+    let(:params) { {'package_version' => 'CustomVersion.exe', 'package_source_location' => 'http://myproxy.com:8080' } }
+
+    it { should contain_package('BigFix Enterprise Client')
+      .with_ensure('installed')
+      .with_provider('windows')
+      .with_source('http://myproxy.com:8080/CustomVersion.exe')
+    }
+
+  end
+
+  context 'when trying to install on Ubuntu' do
+    let(:facts) { {:osfamily => 'Ubuntu'} }
+    it do
+      expect {
+        should contain_class('bigfix')
+      }.to raise_error(Puppet::Error, /^This module only works on Windows based systems./)
+    end
+  end
+
+end
